9b2ed439971bfb1a6274a2dc6ff365c92282005d
David Arthur <mumrah@gmail.com>
2012-01-19 21:47:46 -0500

    Removing wrapper class, just returns byte[] now
    Much, much faster now.
    
    Also added eclipse files

--- src/uniq/UniqueId.java
+++ src/uniq/UniqueId.java
@@ -2,38 +2,10 @@ package uniq;
 
 import java.nio.ByteBuffer;
 
-import org.apache.commons.codec.binary.Hex;
-
 import com.eaio.uuid.UUIDGen;
 
 public class UniqueId {
-    public static class IdStruct {
-        public final long time;
-        public final byte[] node;
-        public final short seq;
-        private final byte[] bytes;
-        
-        public IdStruct(long time, byte[] node, short seq) {
-            this.time = time;
-            this.node = node;
-            this.seq = seq;
-            
-            ByteBuffer bb = ByteBuffer.allocate(16);
-            bb.putLong(time);
-            bb.put(node);
-            bb.putShort(seq);
-            this.bytes = bb.array();
-        }
-        
-        public String toString() {
-            return String.format("%011x-%s-%04x", time, Hex.encodeHexString(node), seq);
-        }
-        
-        public byte[] toBytes() {
-            return bytes;
-        }
-    }
-    
+  
     // Get the MAC address (i.e., the "node" from a UUID1)
     private final long clockSeqAndNode = UUIDGen.getClockSeqAndNode();
     private final byte[] node = new byte[]{
@@ -48,13 +20,14 @@ public class UniqueId {
     private volatile short seq;
     private volatile long lastTimestamp;
     private final Object lock = new Object();
-
-    public IdStruct getId() {
-        long time;
+        
+    public byte[] getId() {
+        long time = 0;
         synchronized(lock) {
             time = System.currentTimeMillis();
             if(time != lastTimestamp) {
                 lastTimestamp = time;
+                System.err.println(Thread.currentThread().getId() + " " + seq);
                 seq = 0;
                 // We're cutting the range of seq in half by counting from zero
                 // but it makes for nicer looking IDs. Also, do we really expect
@@ -68,7 +41,11 @@ public class UniqueId {
             }
             seq++;
         }
-        return new IdStruct(time, node, seq);
+        ByteBuffer bb = ByteBuffer.allocate(16);
+        bb.putLong(time);
+        bb.put(node);
+        bb.putShort(seq);
+        return bb.array();    
     }
 
     public static void main(String[] args) {
